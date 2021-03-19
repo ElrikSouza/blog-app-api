5b2c8570de4c7968cfe3c0a1338eb633742b861c
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-03-19 18:11:47 -0400

    changed main.ts to use fastify instead of express

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'fastify-helmet';
+import { envVars } from './env';
+import {
+  NestFastifyApplication,
+  FastifyAdapter,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.register(helmet);
+
+  await app.listen(envVars.PORT);
 }
+
 bootstrap();
