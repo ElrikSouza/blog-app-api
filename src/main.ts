58cee5de8efbcf4b687aa45deb7eeb84b25d8a60
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-03-20 21:27:11 -0400

    Updated the app's configuration
    - Added a compression middleware
    - Added a global validation pipe
    - Changed the default parser of BigInts (postgresql)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'fastify-helmet';
+import compress from 'fastify-compress';
 import { envVars } from './env';
 import {
   NestFastifyApplication,
   FastifyAdapter,
 } from '@nestjs/platform-fastify';
+import { types } from 'pg';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -13,7 +16,13 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
+  // Postgres Driver: Use integers instead of strings
+  types.setTypeParser(20, Number.parseInt);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   app.register(helmet);
+  app.register(compress, { encodings: ['gzip', 'deflate'] });
 
   await app.listen(envVars.PORT);
 }
