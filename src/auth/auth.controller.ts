72bafa4ec62ae7e8d291c855a7e00c36fa032c74
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-03-26 18:05:23 -0400

    created auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { AuthDTO } from './auth.dto';
+import { AuthService } from './auth.service';
+import { FastifyReply } from 'fastify';
+import { CookieSerializeOptions } from 'fastify-cookie';
+import { envVars } from 'src/env';
+
+const cookieOptions: CookieSerializeOptions = {
+  maxAge: 172800,
+  sameSite: true,
+  signed: true,
+  secure: true,
+  domain: envVars.COOKIE_DOMAIN,
+};
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  public async logIn(
+    @Res({ passthrough: true }) response: FastifyReply,
+    @Body() userInfo: AuthDTO,
+  ) {
+    const accessToken = await this.authService.logIn(userInfo);
+
+    return response
+      .setCookie('Authorization', accessToken, {
+        ...cookieOptions,
+        httpOnly: true,
+      })
+      .setCookie('isLoggedIn', 'true', cookieOptions)
+      .status(200)
+      .send({ accessToken });
+  }
+
+  @Post('sign-up')
+  public async signUp(@Body() userInfo: AuthDTO) {
+    await this.authService.signUp(userInfo);
+
+    return { message: 'User account has been created' };
+  }
+}
