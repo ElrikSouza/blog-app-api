cad467d2e0c1c5a14092d598e8c5479457f2f861
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-03-31 11:50:34 -0400

    updated auth.controller.spec.ts

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,11 +1,27 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CrpytModule } from 'crpyt/crpyt.module';
+import { UserAccount } from 'user-account/user-account.entity';
+import { mockUserAccountRepo } from 'user-account/user-account.mock';
+import { UserAccountService } from 'user-account/user-account.service';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        UserAccountService,
+        {
+          provide: getRepositoryToken(UserAccount),
+          useValue: mockUserAccountRepo,
+        },
+      ],
+      imports: [JwtModule.register({ secret: 'test' }), CrpytModule],
       controllers: [AuthController],
     }).compile();
 
