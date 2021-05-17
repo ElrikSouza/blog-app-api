bd02bc182950c795718a343d954598d2b7d79dcd
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-05-17 14:39:40 -0400

    created ProfileModule (getOneProfile is the only implemented method so far)

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ProfileNameValidationPipe } from './profile-name.pipe';
+import { ProfileService } from './profile.service';
 
-@Controller('profile')
-export class ProfileController {}
+@Controller('profiles')
+export class ProfileController {
+  constructor(private profileService: ProfileService) {}
+
+  @Get(':profile')
+  public async getOneProfile(
+    @Param('profile', ProfileNameValidationPipe) profileName: string,
+  ) {
+    const profile = await this.profileService.getProfileByName(profileName);
+
+    return profile;
+  }
+}
--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserProfile } from './user-profile.entity';
 
 @Module({
   providers: [ProfileService],
-  controllers: [ProfileController]
+  imports: [TypeOrmModule.forFeature([UserProfile])],
+  controllers: [ProfileController],
 })
 export class ProfileModule {}
