30e4229ad326a208225929d6f71ace2c5539b863
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-05-16 22:08:04 -0400

    created function to generate sample user accounts using random data

--- src/user-account/user-account.mock.ts
+++ src/user-account/user-account.mock.ts
@@ -1,5 +1,6 @@
 import { v4 as uuid } from 'uuid';
 import { hashSync } from 'bcrypt';
+import * as faker from 'faker';
 
 export const mockUserAccounts = [
   {
@@ -13,6 +14,27 @@ export const mockUserAccounts = [
     password: hashSync('12345678', 1),
   },
 ];
+const hashed1to8 = hashSync('12345678', 1);
+
+const createOneMockUserAccount = (password = hashed1to8) => ({
+  id: faker.datatype.uuid(),
+  email: faker.internet.email(),
+  password,
+});
+
+export const createMockUserAccounts = (amount: number) => {
+  if (amount <= 0) {
+    throw new Error('Amount of generated accounts <= 0');
+  }
+
+  const mockUsers = [createOneMockUserAccount()];
+
+  for (let i = 1; i < amount; i++) {
+    mockUsers.push(createOneMockUserAccount());
+  }
+
+  return mockUsers;
+};
 
 export const mockUserAccountRepo = {
   async count({ email }: { email: string }) {
