949c0353af69de3c83fba5365c69017652fd1d66
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-05-16 22:08:36 -0400

    replaced hard coded test values with random data

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,26 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { AuthModule } from 'auth/auth.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import cookies from 'fastify-cookie';
-import { ValidationPipe } from '@nestjs/common';
 import { EntityManager } from 'typeorm';
 import { UserAccount } from 'user-account/user-account.entity';
 import type { InjectOptions } from './inject-options.type';
 import { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { createModuleFixture, createTestApp } from './module-fixture';
+import * as faker from 'faker';
+import { createMockUserAccounts } from 'user-account/user-account.mock';
 
 describe('AuthController (e2e)', () => {
   let app: NestFastifyApplication;
   let entityManager: EntityManager;
 
-  const userAccounts = [
-    {
-      id: 'e7ce7423-e750-4b6e-bf65-616adaed92fd',
-      email: 'example@example.com',
-      //12345678
-      password: '$2b$04$7Ws/g16YVokTY5B7lC4kMONKzXyWmpWvm2ppA6sjuA7RUQxvTLTcK',
-    },
-  ];
+  const userAccounts = createMockUserAccounts(1);
+  const correctPassword = '12345678';
 
   beforeAll(async () => {
     const moduleFixture = await createModuleFixture([AuthModule]);
@@ -43,7 +35,7 @@ describe('AuthController (e2e)', () => {
     it('should succeed if the credentials are correct', async () => {
       const result = await app.inject({
         ...loginPost,
-        payload: { email: 'example@example.com', password: '12345678' },
+        payload: { email: userAccounts[0].email, password: correctPassword },
       });
 
       expect(JSON.parse(result.body)).toHaveProperty('accessToken');
@@ -54,7 +46,10 @@ describe('AuthController (e2e)', () => {
     it('should return 400 if the provided data is invalid', async () => {
       const result = await app.inject({
         ...loginPost,
-        payload: { email: 'example', password: '1' },
+        payload: {
+          email: faker.datatype.string(1),
+          password: faker.datatype.string(4),
+        },
       });
 
       expect(result.statusCode).toBe(400);
@@ -63,7 +58,10 @@ describe('AuthController (e2e)', () => {
     it('should return 401 if the password is wrong', async () => {
       const result = await app.inject({
         ...loginPost,
-        payload: { email: 'example@example.com', password: 'wrongpassword' },
+        payload: {
+          email: userAccounts[0].email,
+          password: faker.datatype.string(9),
+        },
       });
 
       expect(result.statusCode).toBe(401);
@@ -72,7 +70,10 @@ describe('AuthController (e2e)', () => {
     it('should return 401 if the requested account does not exist', async () => {
       const result = await app.inject({
         ...loginPost,
-        payload: { email: 'notfound@example.com', password: '12345678' },
+        payload: {
+          email: faker.internet.email(),
+          password: faker.datatype.string(8),
+        },
       });
 
       expect(result.statusCode).toBe(401);
@@ -85,7 +86,10 @@ describe('AuthController (e2e)', () => {
     it('should return 400 if the provided email is already taken', async () => {
       const result = await app.inject({
         ...signUpPost,
-        payload: { email: 'example@example.com', password: '12345678' },
+        payload: {
+          email: userAccounts[0].email,
+          password: faker.datatype.string(8),
+        },
       });
 
       expect(result.statusCode).toBe(400);
@@ -94,7 +98,10 @@ describe('AuthController (e2e)', () => {
     it('should return 400 if the provided data is invalid', async () => {
       const result = await app.inject({
         ...signUpPost,
-        payload: { email: 'aa', password: 'aa' },
+        payload: {
+          email: faker.datatype.string(2),
+          password: faker.datatype.string(2),
+        },
       });
 
       expect(result.statusCode).toBe(400);
@@ -103,7 +110,10 @@ describe('AuthController (e2e)', () => {
     it('should succeed if the email is available and the provided data is valid', async () => {
       const result = await app.inject({
         ...signUpPost,
-        payload: { email: 'aaa@email.com', password: 'aaaaaaaaaaa' },
+        payload: {
+          email: faker.internet.email(),
+          password: faker.datatype.string(9),
+        },
       });
 
       expect(result.statusCode).toBe(201);
