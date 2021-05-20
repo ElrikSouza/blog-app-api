d43f5a52976dd2a03e4c86996e706528a0e76a5b
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-05-20 15:04:01 -0400

    created a POST /profiles endpoint for creating profiles

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Delete, Get, Param, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'auth/jwt/jwt.guard';
 import { UserId } from 'auth/jwt/userid.decorator';
+import { CreateProfileDTO } from './create-profile.dto';
 import { ProfileNameValidationPipe } from './profile-name.pipe';
 import { ProfileService } from './profile.service';
 
 @Controller('profiles')
 export class ProfileController {
   constructor(private profileService: ProfileService) {}
+
+  @Post('')
+  @UseGuards(JwtAuthGuard)
+  public async createProfile(
+    @Body() profileData: CreateProfileDTO,
+    @UserId() userId: string,
+  ) {
+    await this.profileService.createProfile(profileData, userId);
+  }
 
   @Get(':profile')
   public async getOneProfile(
