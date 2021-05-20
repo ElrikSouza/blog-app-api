fcbaca935c218ed315b6c14c86f1aaa2d0828ccf
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-05-20 12:14:32 -0400

    fixed CreatedProfileDTO's definition

--- src/profile/create-profile.dto.ts
+++ src/profile/create-profile.dto.ts
@@ -1,15 +1,13 @@
 import { Optional } from '@nestjs/common';
 import { IsString, Length } from 'class-validator';
 
-export class EditProfileDTO {
+export class CreateProfileDTO {
   @IsString()
-  @Length(2, 35)
-  @Optional()
+  @Length(2, 30)
   display_name?: string;
 
   @IsString()
-  @Length(2, 35)
-  @Optional()
+  @Length(2, 30)
   profile_name?: string;
 
   @IsString()
